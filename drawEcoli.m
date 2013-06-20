417223608ea9b21822cd9d6a8e5263852ac4c2ee
Stephen Wilson <stephen.wilson2@bcm.edu>
2013-06-20 13:30:57 -0500

    Revert "started 3-d, going to fork repo to keep 2-d program"
    This reverts commit 21239a6bce8329f0eca203fef84a2113baed9ab0.

--- drawEcoli.m
+++ drawEcoli.m
@@ -18,14 +18,15 @@ function imgdata=drawEcoli (img,varargin)
 
     %Default values
     num = 5;
-    l = 10;
-    r = 30;
+    len = 10;
+    height = 30;
     steps = 1000;
     fix='no';
-    imsize=size(img{1},1);
+    t=size(img);
+    imsize=t(1);
     
     % Sets defaults for optional inputs
-    optargs = {num,r,l,steps,fix};
+    optargs = {num,height,len,steps,fix};
     
     %Checks to ensure  3 optional inputs at most
     numvarargs = length(varargin);
@@ -36,26 +37,26 @@ function imgdata=drawEcoli (img,varargin)
     % Overwrites defaults if optional input exists
     optargs(1:numvarargs) = varargin;
     num = cell2mat(optargs(1));
-    l = cell2mat(optargs(3));
-    r = cell2mat(optargs(2));
+    len = cell2mat(optargs(3));
+    height = cell2mat(optargs(2));
     steps = cell2mat(optargs(4));
     fix = cell2mat(optargs(5));
     
     % Fixes or randomizes the locations of the cells
     
-    if l<r
-        tmp=r;
-        r=l;
-        l=tmp;
+    if len<height
+        tmp=height;
+        height=len;
+        len=tmp;
     end
     
     if strcmp(fix, 'no')== 1
         n=[0 0];
         while n(1)<num
-            randpair=randi([r imsize-r],1,2); %gets a random origin for a cell
+            randpair=randi([height imsize-height],1,2); %gets a random origin for a cell
             ang = random('normal',180,90,1,1);
             q={randpair, ang};
-            imgls=draw(img,q,l,r,steps);
+            imgls=draw(img,q,len,height,steps);
             img=imgls{1};
             if imgls{2}~=1 && n(1)==0
                 w=randpair;
@@ -71,8 +72,8 @@ function imgdata=drawEcoli (img,varargin)
         end
     else % Linearly spaces the cells on a diagnol in predicable places
         f=num/5+3;
-        l=linspace(r,imsize-r,num+f);
-        l2=linspace(imsize-r,r,num+f);
+        l=linspace(height,imsize-height,num+f);
+        l2=linspace(imsize-height,height,num+f);
         randpair=[l2;l];
         randpair=randpair';
         ang(1:uint16(num+f))=30;
@@ -81,7 +82,7 @@ function imgdata=drawEcoli (img,varargin)
         while n(1)<num && i<floor(num+f)
             i=i+1;
             q={randpair(i,:), ang(i)};
-            imgls=draw(img,q,l,r,steps);
+            imgls=draw(img,q,len,height,steps);
             img=imgls{1};
             if imgls{2}~=1 && n(1)==0
                 w=randpair(i,:);
@@ -97,63 +98,52 @@ function imgdata=drawEcoli (img,varargin)
         end
 
     end
-%     %imagesc(img);
-%     imgdata=true;
-%     if strcmp(fix, 'no')== 0
-%         if i>=floor(num+f)
-%             'could not draw'
-%             imgdata=false;
-%         end
-%     end
-%     if imgdata~=false
-%         %Create individual cells in a cell list
-         dim={l,r};
-%         u(l,r)=0;
-%         calc2 = uint32(calculateEllipse(l/2, r/2, l/2, r/2, 0, steps));
-%         calc2(calc2==0)=1;
-%         calc2=calc2';
-%         for calc =calc2
-%             u(calc(1),calc(2))=1;
-%         end
-%         u=imfill(u,'holes');
-%         for i=(1:length(a))
-%             cells(i)={u};
-%         end
-%         cells=cells';
-        %Pass all information the the form of a cell list
-        for p=1:length(img)
-            figure(p);imagesc(img{p});
+    %imagesc(img);
+    imgdata=true;
+    if strcmp(fix, 'no')== 0
+        if i>=floor(num+f)
+            'could not draw'
+            imgdata=false;
         end
-        imgdata={img,a,w,dim};%,cells};
-%     end
+    end
+    if imgdata~=false
+        %Create individual cells in a cell list
+        dim={len,height};
+        u(len,height)=0;
+        calc2 = uint32(calculateEllipse(len/2, height/2, len/2, height/2, 0, steps));
+        calc2(calc2==0)=1;
+        calc2=calc2';
+        for calc =calc2
+            u(calc(1),calc(2))=1;
+        end
+        u=imfill(u,'holes');
+        for i=(1:length(a))
+            cells(i)={u};
+        end
+        cells=cells';
+        %Pass all information the the form of a cell list
+        
+        imgdata={img,a,w,dim,cells};
+    end
     
 
 end
 
-function imgls=draw(img,num,l,r,s)
+function imgls=draw(img,num,l,h,s)
 %%Draws the cells. Takes the image and num, a cell with the coordinate pair 
 %%in the first cell and the angles in the second. If redraw needed, it
 %%returns a 1.
-    xc=r;
-    yc=r;
-    zc=l;
-    [x,y,z]=ellipsoid(xc,yc,zc,r,r,l,s);
-    
-    x=round(x);
-    y=round(y);
-    z=round(z);
-
+    calc = calculateEllipse(num{1}(1), num{1}(2), l/2, h/2, num{2}, ...
+        s);
+    calc=uint32(calc);
     bi=img;
     redo=0;
-    img=checkShape(img,{x,y,z});
-    if img{1}==0
+    img=checkShape(img,calc);
+    if img==0
         redo=1;
         img=bi;
     else
-        for p=1:length(img)
-            img{p}=sparse(imfill(full(img{p}),'holes'));
-            % figure(p);imagesc(k{p});
-        end
+        img=imfill(img,'holes');
     end
     imgls={img,redo};
 end
@@ -164,46 +154,36 @@ function img=checkShape(img, pts)
 %%and pts, coordinate pairs about the location of the cells
 
     n=0;
-    imsize=size(img{1},1);
-    x=pts{1};
-    y=pts{2};
-    z=pts{3};
-    for i=1:size(x,1)
-        for j=1:size(x,1)
-            if x(i,j)+1 == 0 || y(i,j)+1 == 0 || z(i,j)+1==0 %exceeds the bounds of the image
-                n=n+1;
-            elseif x(i,j)+1 >= imsize || y(i,j)+1 >= imsize || z(i,j)+1>=imsize %exceeds the bounds of the image
-                n=n+1;
-            end
+    t=size(img);
+    imsize=t(1);
+    for pt = pts'
+        x=pt(1);
+        y=pt(2);
+        if x == 0 || y == 0 %exceeds the bounds of the image
+            n=n+1;
+        elseif x >= imsize || y >= imsize %exceeds the bounds of the image
+            n=n+1;
         end
     end
-
     
-    %funtion that transfers coord over for 3d
-    for j=1:size(z,1)
-        for e=1:size(z,1)
-            img{z(j,e)+1}(x(j,e)+1,y(j,e)+1)=1;
+    if n==0
+        A = uint16(zeros(imsize,imsize));
+        for pair = pts'
+            if pair(1)~=0 && pair(2)~=0 && pair(1)<imsize && pair(2)<imsize
+                A(pair(1),pair(2)) = 1;
+            end
         end
-    end 
+        b = img(A==1);
+        s= sum(b);
+        if s>0
+            'There is overlap!';
+        end
+        
+        img(A==1) = 1;
+
+    end
     
-%     if n==0
-%         A = uint16(zeros(imsize,imsize));
-%         for pair = pts'
-%             if pair(1)~=0 && pair(2)~=0 && pair(1)<imsize && pair(2)<imsize
-%                 A(pair(1),pair(2)) = 1;
-%             end
-%         end
-%         b = img(A==1);
-%         s= sum(b);
-%         if s>0
-%             'There is overlap!';
-%         end
-%         
-%         img(A==1) = 1;
-% 
-%     end
-%     
-    if n>0 %|| s>0
-        img{1}=0;
+    if n>0 || s>0
+        img=0;
     end
 end
